7fae17e71cbd07ce7033916f46385d2cad3b72e5
Bilal <mskenmasters@hotmail.com>
2023-06-16 19:07:13 +0200

    actualiza actividad 1

--- actividad1.ts
+++ actividad1.ts
@@ -1,4 +1,5 @@
 function ejecutar() {
+	// Obtener el elemento del input y de la tabla de multiplicar
 	const inputNumero = document.getElementById(
 		"inputNumero"
 	) as HTMLInputElement;
@@ -9,23 +10,25 @@ function ejecutar() {
 	// Obtén el número ingresado por el usuario como tipo number
 	const numero: number = Number(inputNumero.value);
 
+	// Validar si el número ingresado es válido
 	if (isNaN(numero)) {
-		alert("Por favor, ingresa un número válido.");
-		return;
+		return; // Detener la ejecución de la función si el número no es válido
 	}
 
-	tablaMultiplicar.innerHTML = "";
+	tablaMultiplicar.innerHTML = ""; // Limpiar la tabla de multiplicar antes de generar nuevos resultados
 
-	const resultados: number[] = [];
+	const resultados: number[] = []; // Crear un array para almacenar los resultados
 
+	// Generar la tabla de multiplicar y los elementos de lista correspondientes
 	for (let i = 1; i <= 10; i++) {
 		const resultado = numero * i;
-		resultados.push(resultado);
+		resultados.push(resultado); // Agregar el resultado al array de resultados
 
+		// Crear un elemento <li> para cada resultado y agregarlo a la tabla de multiplicar
 		const listItem = document.createElement("li");
 		listItem.textContent = `${numero}x${i}=${resultado}`;
 		tablaMultiplicar.appendChild(listItem);
 	}
 
-	console.log(resultados);
+	console.log(resultados); // Mostrar los resultados en la consola
 }

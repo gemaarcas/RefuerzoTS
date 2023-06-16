13a4f2b555651460c6fcfdd3207cb2ad11b443a9
piscinagua <correo_javi@hotmail.com>
2023-06-16 19:39:43 +0200

    Actualiza actividad3 con las url y los textos alternativos de 4 imágenes.

--- actividad3.ts
+++ actividad3.ts
@@ -6,7 +6,24 @@ interface IFoto {
 	alt: string;
 }
 
-let imagenes: IFoto[] = [];
+let imagenes: IFoto[] = [
+	{
+		url: "https://img.freepik.com/vector-gratis/vector-diseno-degradado-colorido-pajaro_343694-2506.jpg",
+		alt: "Pájaro 1 colorido",
+	},
+	{
+		url: "https://img.freepik.com/vetores-gratis/vetor-de-gradiente-de-logotipo-colorido-de-passaro_343694-1365.jpg?size=626&ext=jpg",
+		alt: "pájaro 2 colorido",
+	},
+	{
+		url: "https://img.freepik.com/vetores-premium/ilustracao-colorida-do-passaro-do-inclinacao_84302-458.jpg",
+		alt: "Pájaro 3 colorido",
+	},
+	{
+		url: "https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon.png",
+		alt: "Logo de React",
+	},
+];
 
 const visualizarFoto = () => {
 	contenedorFoto.innerHTML = "";

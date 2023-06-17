3dd444433f2293a148cbe8822ac29ade345277cf
Bilal <mskenmasters@hotmail.com>
2023-06-17 11:56:14 +0200

    añade actividad4.html y modificar actividad4.ts para su funcionamiento con el html, he incluido un añadido en el archivo ts para que los resultados se vean en euros

---
+++ js/actividad4.js
@@ -0,0 +1,49 @@
+"use strict";
+class Facturas {
+    constructor(importe, porcentajeDescuento) {
+        this.importe = importe;
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+    setImporte(importe) {
+        this.importe = importe;
+    }
+    setPorcentajeDescuento(porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+    getImporte() {
+        return this.importe;
+    }
+    getPorcentajeDescuento() {
+        return this.porcentajeDescuento;
+    }
+    getBruto() {
+        return this.importe - (this.importe * this.porcentajeDescuento) / 100;
+    }
+    getIVA() {
+        return (this.getBruto() * 21) / 100;
+    }
+}
+function calcularFactura() {
+    const importe = parseFloat(document.getElementById("importe").value);
+    const factura = new Facturas(importe, 10);
+    const bruto = factura.getBruto();
+    const iva = factura.getIVA();
+    const neto = bruto + iva;
+    const brutoEuro = bruto.toLocaleString("es-ES", {
+        style: "currency",
+        currency: "EUR",
+    });
+    const ivaEuro = iva.toLocaleString("es-ES", {
+        style: "currency",
+        currency: "EUR",
+    });
+    const netoEuro = neto.toLocaleString("es-ES", {
+        style: "currency",
+        currency: "EUR",
+    });
+    const resultadoElement = document.getElementById("resultado");
+    if (resultadoElement) {
+        resultadoElement.innerText = `Bruto: ${brutoEuro}, IVA: ${ivaEuro}, Neto: ${netoEuro}`;
+    }
+}
+//# sourceMappingURL=actividad4.js.map
